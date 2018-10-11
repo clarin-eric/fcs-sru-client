fea1bfc74e44df90ff5b91c0de722361356c84b2
benhur1999 <benhur1999@users.noreply.github.com>
2018-10-11 11:58:05 +0000

    - get rid of static builder() method in SRUClientConfig.Builder

--- src/main/java/eu/clarin/sru/client/SRUClientConfig.java
+++ src/main/java/eu/clarin/sru/client/SRUClientConfig.java
@@ -354,16 +354,6 @@ public class SRUClientConfig {
             extraDataParsers.add(parser);
             return this;
         }
-
-
-        /**
-         * Create a configuration instance object for configuring SRU clients
-         *
-         * @return a immutable configuration instance
-         */
-        public SRUClientConfig build() {
-            return new SRUClientConfig(this);
-        }
     } // inner class Builder
 
 } // class SRUClientConfig
